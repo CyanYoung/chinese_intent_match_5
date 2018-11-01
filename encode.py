89a3a4078368094d5f15dad9a525a01111447062
yangjc <bupt_yjc@163.com>
2018-11-01 11:33:12 +0800

    add cluster

--- encode.py
+++ encode.py
@@ -1,12 +1,16 @@
 import pickle as pk
 
+import numpy as np
+
 from keras.models import Model
 from keras.layers import Input, Embedding
 from keras.utils import plot_model
 
+from sklearn.cluster import KMeans
+
 from nn_arch import dnn_cache, cnn_cache, rnn_cache
 
-from util import map_item
+from util import flat_read, map_item
 
 
 def define_model(name, embed_mat, seq_len):
@@ -29,13 +33,16 @@ def load_model(name, embed_mat, seq_len):
 
 
 seq_len = 30
+max_core = 5
 
 path_embed = 'feat/embed.pkl'
 path_sent = 'feat/sent_train.pkl'
+path_train = 'data/train.csv'
 with open(path_embed, 'rb') as f:
     embed_mat = pk.load(f)
 with open(path_sent, 'rb') as f:
     sents = pk.load(f)
+labels = flat_read(path_train, 'label')
 
 funcs = {'dnn': dnn_cache,
          'cnn': cnn_cache,
@@ -44,6 +51,9 @@ funcs = {'dnn': dnn_cache,
 paths = {'dnn': 'model/dnn.h5',
          'cnn': 'model/cnn.h5',
          'rnn': 'model/rnn.h5',
+         'dnn_cache': 'cache/dnn.h5',
+         'cnn_cache': 'cache/cnn.h5',
+         'rnn_cache': 'cache/rnn.h5',
          'dnn_plot': 'model/plot/dnn_cache.png',
          'cnn_plot': 'model/plot/cnn_cache.png',
          'rnn_plot': 'model/plot/rnn_cache.png'}
@@ -53,17 +63,45 @@ models = {'dnn': load_model('dnn', embed_mat, seq_len),
           'rnn': load_model('rnn', embed_mat, seq_len)}
 
 
-def cache(name, sents, path_cache):
-    model = map_item(name, models)
-    encode_sents = model.predict(sents)
-    with open(path_cache, 'wb') as f:
-        pk.dump(encode_sents, f)
+def label2ind(labels, path_label_ind):
+    label_set = sorted(list(set(labels)))
+    label_inds = dict()
+    for i in range(len(label_set)):
+        label_inds[label_set[i]] = i
+    inds = [label_inds[label] for label in labels]
+    with open(path_label_ind, 'wb') as f:
+        pk.dump(label_inds, f)
+    return label_inds, np.array(inds)
+
+
+def cluster(sents, inds, ind_labels):
+    label_num = len(ind_labels)
+    cores = list()
+    labels = list()
+    for i in range(label_num):
+        ind_args = np.where(inds == i)
+        match_sents = sents[ind_args]
+        core_num = min(len(match_sents), max_core)
+        model = KMeans(n_clusters=core_num, n_init=10, max_iter=100)
+        model.fit(match_sents)
+        cores.extend(model.cluster_centers_.tolist())
+        labels.extend([ind_labels[i]] * core_num)
+    return np.array(cores), labels
+
+
+def cache(sents, labels, path_label_ind):
+    label_inds, inds = label2ind(labels, path_label_ind)
+    ind_labels = dict()
+    for label, ind in label_inds.items():
+        ind_labels[ind] = label
+    for name, model in models.items():
+        path_cache = map_item(name + '_cache', paths)
+        encode_sents = model.predict(sents)
+        cores_labels = cluster(encode_sents, inds, ind_labels)
+        with open(path_cache, 'wb') as f:
+            pk.dump(cores_labels, f)
 
 
 if __name__ == '__main__':
-    path_cache = 'cache/dnn.pkl'
-    cache('dnn', sents, path_cache)
-    path_cache = 'cache/cnn.pkl'
-    cache('cnn', sents, path_cache)
-    path_cache = 'cache/rnn.pkl'
-    cache('rnn', sents, path_cache)
+    path_label_ind = 'feat/label_ind.pkl'
+    cache(sents, labels, path_label_ind)
