f87022720d785eec152af68acf978c5f7b52b19f
yangjc <bupt_yjc@163.com>
2019-02-25 16:55:49 +0800

    sync change

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.utils import plot_model
 
-from nn_arch import dnn, cnn, rnn
+from nn_arch import dnn, cnn_1d, cnn_2d, rnn
 
 from util import map_item
 
@@ -15,23 +15,26 @@ batch_size = 128
 
 path_embed = 'feat/embed.pkl'
 path_pair = 'feat/pair_train.pkl'
-path_flag = 'feat/flag_train.pkl'
+path_label = 'feat/label_train.pkl'
 with open(path_embed, 'rb') as f:
     embed_mat = pk.load(f)
 with open(path_pair, 'rb') as f:
     pairs = pk.load(f)
-with open(path_flag, 'rb') as f:
-    flags = pk.load(f)
+with open(path_label, 'rb') as f:
+    labels = pk.load(f)
 
 funcs = {'dnn': dnn,
-         'cnn': cnn,
+         'cnn_1d': cnn_1d,
+         'cnn_2d': cnn_2d,
          'rnn': rnn}
 
 paths = {'dnn': 'model/dnn.h5',
-         'cnn': 'model/cnn.h5',
+         'cnn_1d': 'model/cnn_1d.h5',
+         'cnn_2d': 'model/cnn_2d.h5',
          'rnn': 'model/rnn.h5',
          'dnn_plot': 'model/plot/dnn.png',
-         'cnn_plot': 'model/plot/cnn.png',
+         'cnn_1d_plot': 'model/plot/cnn_1d.png',
+         'cnn_2d_plot': 'model/plot/cnn_2d.png',
          'rnn_plot': 'model/plot/rnn.png'}
 
 
@@ -48,20 +51,21 @@ def compile(name, embed_mat, seq_len):
     model = Model([input1, input2], output)
     model.summary()
     plot_model(model, map_item(name + '_plot', paths), show_shapes=True)
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
     return model
 
 
-def fit(name, epoch, embed_mat, pairs, flags):
+def fit(name, epoch, embed_mat, pairs, labels):
     sent1s, sent2s = pairs
     seq_len = len(sent1s[0])
     model = compile(name, embed_mat, seq_len)
     check_point = ModelCheckpoint(map_item(name, paths), monitor='val_loss', verbose=True, save_best_only=True)
-    model.fit([sent1s, sent2s], flags, batch_size=batch_size, epochs=epoch,
+    model.fit([sent1s, sent2s], labels, batch_size=batch_size, epochs=epoch,
               verbose=True, callbacks=[check_point], validation_split=0.2)
 
 
 if __name__ == '__main__':
-    fit('dnn', 10, embed_mat, pairs, flags)
-    fit('cnn', 10, embed_mat, pairs, flags)
-    fit('rnn', 10, embed_mat, pairs, flags)
+    fit('dnn', 10, embed_mat, pairs, labels)
+    fit('cnn_1d', 10, embed_mat, pairs, labels)
+    fit('cnn_2d', 10, embed_mat, pairs, labels)
+    fit('rnn', 10, embed_mat, pairs, labels)
--- nn_arch.py
+++ nn_arch.py
@@ -1,89 +1,95 @@
-from keras.layers import Dense, SeparableConv1D, LSTM
-from keras.layers import GlobalMaxPooling1D, Masking, Lambda, Dot, Concatenate
+from keras.layers import Dense, SeparableConv1D, SeparableConv2D, LSTM
+from keras.layers import Dropout, GlobalMaxPooling1D, MaxPooling2D, Masking, Lambda
+from keras.layers import Concatenate, Flatten, Reshape, Subtract, Multiply, Dot
 
 import keras.backend as K
 
 
+seq_len = 30
+
+
 def dnn(embed_input1, embed_input2):
-    mean = Lambda(lambda a: K.mean(a, axis=1), name='mean')
-    da1 = Dense(200, activation='relu', name='encode1')
-    da2 = Dense(200, activation='relu', name='encode2')
+    mean = Lambda(lambda a: K.mean(a, axis=1))
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(200, activation='relu')
+    da3 = Dense(200, activation='relu')
+    da4 = Dense(1, activation='sigmoid')
     x = mean(embed_input1)
     x = da1(x)
     x = da2(x)
     y = mean(embed_input2)
     y = da1(y)
     y = da2(y)
-    return Dot(1, normalize=True)([x, y])
+    diff = Lambda(lambda a: K.abs(a))(Subtract()([x, y]))
+    prod = Multiply()([x, y])
+    z = Concatenate()([x, y, diff, prod])
+    z = da3(z)
+    z = Dropout(0.2)(z)
+    return da4(z)
 
 
-def dnn_encode(embed_input):
-    mean = Lambda(lambda a: K.mean(a, axis=1), name='mean')
-    da1 = Dense(200, activation='relu', name='encode1')
-    da2 = Dense(200, activation='relu', name='encode2')
-    x = mean(embed_input)
-    x = da1(x)
-    return da2(x)
-
-
-def cnn(embed_input1, embed_input2):
-    ca1 = SeparableConv1D(filters=64, kernel_size=1, padding='same', activation='relu', name='conv1')
-    ca2 = SeparableConv1D(filters=64, kernel_size=2, padding='same', activation='relu', name='conv2')
-    ca3 = SeparableConv1D(filters=64, kernel_size=3, padding='same', activation='relu', name='conv3')
+def cnn_1d(embed_input1, embed_input2):
+    ca1 = SeparableConv1D(filters=64, kernel_size=1, padding='same', activation='relu')
+    ca2 = SeparableConv1D(filters=64, kernel_size=2, padding='same', activation='relu')
+    ca3 = SeparableConv1D(filters=64, kernel_size=3, padding='same', activation='relu')
     mp = GlobalMaxPooling1D()
-    concat = Concatenate()
-    da = Dense(200, activation='relu', name='encode')
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(200, activation='relu')
+    da3 = Dense(1, activation='sigmoid')
     x1 = ca1(embed_input1)
     x1 = mp(x1)
     x2 = ca2(embed_input1)
     x2 = mp(x2)
     x3 = ca3(embed_input1)
     x3 = mp(x3)
-    x = concat([x1, x2, x3])
-    x = da(x)
+    x = Concatenate()([x1, x2, x3])
+    x = da1(x)
     y1 = ca1(embed_input2)
     y1 = mp(y1)
     y2 = ca2(embed_input2)
     y2 = mp(y2)
     y3 = ca3(embed_input2)
     y3 = mp(y3)
-    y = concat([y1, y2, y3])
-    y = da(y)
-    return Dot(1, normalize=True)([x, y])
+    y = Concatenate()([y1, y2, y3])
+    y = da1(y)
+    diff = Lambda(lambda a: K.abs(a))(Subtract()([x, y]))
+    prod = Multiply()([x, y])
+    z = Concatenate()([x, y, diff, prod])
+    z = da2(z)
+    z = Dropout(0.2)(z)
+    return da3(z)
 
 
-def cnn_encode(embed_input):
-    ca1 = SeparableConv1D(filters=64, kernel_size=1, padding='same', activation='relu', name='conv1')
-    ca2 = SeparableConv1D(filters=64, kernel_size=2, padding='same', activation='relu', name='conv2')
-    ca3 = SeparableConv1D(filters=64, kernel_size=3, padding='same', activation='relu', name='conv3')
-    mp = GlobalMaxPooling1D()
-    da = Dense(200, activation='relu', name='encode')
-    x1 = ca1(embed_input)
-    x1 = mp(x1)
-    x2 = ca2(embed_input)
-    x2 = mp(x2)
-    x3 = ca3(embed_input)
-    x3 = mp(x3)
-    x = Concatenate()([x1, x2, x3])
-    return da(x)
+def cnn_2d(embed_input1, embed_input2):
+    ca1 = SeparableConv2D(filters=64, kernel_size=2, padding='same', activation='relu')
+    ca2 = SeparableConv2D(filters=64, kernel_size=3, padding='same', activation='relu')
+    mp1 = MaxPooling2D(3)
+    mp2 = MaxPooling2D(5)
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(1, activation='sigmoid')
+    x = Dot(2)([embed_input1, embed_input2])
+    x = Reshape((seq_len, seq_len, 1))(x)
+    x = ca1(x)
+    x = mp1(x)
+    x = ca2(x)
+    x = mp2(x)
+    x = Flatten()(x)
+    x = da1(x)
+    x = Dropout(0.2)(x)
+    return da2(x)
 
 
 def rnn(embed_input1, embed_input2):
-    mask = Masking()
-    ra = LSTM(200, activation='tanh', name='encode1')
-    da = Dense(200, activation='relu', name='encode2')
-    x = mask(embed_input1)
+    ra = LSTM(200, activation='tanh')
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(1, activation='sigmoid')
+    x = Masking()(embed_input1)
     x = ra(x)
-    x = da(x)
-    y = mask(embed_input2)
+    y = Masking()(embed_input2)
     y = ra(y)
-    y = da(y)
-    return Dot(1, normalize=True)([x, y])
-
-
-def rnn_encode(embed_input):
-    ra = LSTM(200, activation='tanh', name='encode1')
-    da = Dense(200, activation='relu', name='encode2')
-    x = Masking()(embed_input)
-    x = ra(x)
-    return da(x)
+    diff = Lambda(lambda a: K.abs(a))(Subtract()([x, y]))
+    prod = Multiply()([x, y])
+    z = Concatenate()([x, y, diff, prod])
+    z = da1(z)
+    z = Dropout(0.2)(z)
+    return da2(z)
