e4fa9c811a7a143ae12efd283f63ef749fa19439
yangjc <bupt_yjc@163.com>
2019-02-26 17:01:44 +0800

    change arch

--- nn_arch.py
+++ nn_arch.py
@@ -5,20 +5,39 @@ import torch.nn as nn
 class Esi(nn.Module):
     def __init__(self, embed_mat):
         super(Esi, self).__init__()
-        self.encode = RnnEncode(embed_mat)
+        self.encode1 = Encode1(embed_mat)
+        self.encode2 = Encode2()
+        self.lal = nn.Sequential(nn.Linear(200, 200),
+                                 nn.ReLU(),
+                                 nn.Linear(200, 200))
+        self.dl = nn.Sequential(nn.Dropout(0.2),
+                                nn.Linear(200, 1))
+
+    def attend(self, x, y):
+        pass
+
+    def merge1(self, x, x_):
+        pass
+
+    def merge2(self, x, y):
+        pass
 
     def forward(self, x, y):
-        x = self.encode(x)
-        y = self.encode(y)
-        return self.match(x, y)
+        x, y = self.encode1(x), self.encode1(y)
+        x_, y_ = self.attend(x, y), self.attend(y, x)
+        x, y = self.merge1(x, x_), self.merge1(y, y_)
+        x, y = self.encode2(x), self.encode2(y)
+        z = self.merge2(x, y)
+        z = self.lal(z)
+        return self.dl(z)
 
 
-class RnnEncode(nn.Module):
+class Encode1(nn.Module):
     def __init__(self, embed_mat):
-        super(RnnEncode, self).__init__()
+        super(Encode1, self).__init__()
         vocab_num, embed_len = embed_mat.size()
-        self.embed = nn.Embedding(vocab_num, embed_len)
-        self.ra = nn.LSTM(embed_len, 200, batch_first=True)
+        self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
+        self.ra = nn.LSTM(embed_len, 200, batch_first=True, bidirectional=True)
 
     def forward(self, x):
         x = self.embed(x)
@@ -26,17 +45,11 @@ class RnnEncode(nn.Module):
         return h[:, -1, :]
 
 
-class Match(nn.Module):
+class Encode2(nn.Module):
     def __init__(self):
-        super(Match, self).__init__()
-        self.la = nn.Sequential(nn.Linear(800, 200),
-                                nn.ReLU())
-        self.dl = nn.Sequential(nn.Dropout(0.2),
-                                nn.Linear(200, 1))
+        super(Encode2, self).__init__()
+        self.ra = nn.LSTM(800, 200, batch_first=True, bidirectional=True)
 
-    def forward(self, x, y):
-        diff = torch.abs(x - y)
-        prod = x * y
-        z = torch.cat([x, y, diff, prod], dim=1)
-        z = self.la(z)
-        return self.dl(z)
+    def forward(self, x):
+        h, hc_n = self.ra(x)
+        return h[:, -1, :]
